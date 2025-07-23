e8a788caa2f046a6942fca9a15946dae0cf47715
Peterthethird96 <piotrkalb963@gmail.com>
2025-07-23 22:34:31 +0200

    updated natebook and modified sql file

--- scripts/Data_Amazon_Analysis.sql
+++ scripts/Data_Amazon_Analysis.sql
@@ -1,10 +1,5 @@
-SELECT * FROM "Ecom_Data_Amazon" eda;
 
-SELECT category,
-max(amount) AS max_amount
-FROM "Ecom_Data_Amazon"
-GROUP BY category
-ORDER BY max_amount DESC;
+SELECT * FROM "Ecom_Data_Amazon" eda;
 
 SELECT column_name, data_type
 FROM information_schema.COLUMNS 
@@ -23,18 +18,17 @@ sales_channel_,
 amount,
 qty,
 category,
-sku
+sku,
 SIZE,
 STYLE,
 ship_city,
 ship_state,
 ship_postal_code
 FROM "Ecom_Data_Amazon"
-WHERE status <> 'Cancelled' AND qty != 0);
+WHERE status <> 'Cancelled' AND qty != 0 AND amount != 0 AND amount IS NOT NULL);
 
--------------------------------------
+-------------------------------------------------------------------------
 DROP TABLE IF EXISTS ecommerce_data;
-
 
 SELECT * FROM ecommerce_data;
 
@@ -44,6 +38,36 @@ FROM ecommerce_data
 GROUP BY qty
 ORDER BY qty DESC;
 
+SELECT count(*)
+FROM ecommerce_data;
+
+-------------------------------------------------------------------------
+# Which products generate high sales but low profits?
+
+CREATE TABLE amazon_margin AS(
+WITH step_1 AS 
+(
+	SELECT STYLE,category,
+	sum(qty) AS quantity_sku,
+	sum(amount) AS amount_per_sku,
+	cost_per_unit
+	FROM ecommerce_data ed
+	GROUP BY STYLE,category,cost_per_unit
+	ORDER BY amount_per_sku ASC),
+step_2 AS (
+	SELECT *, (amount_per_sku-(cost_per_unit*quantity_sku)) AS profit
+	FROM step_1),
+step_3 AS (
+	SELECT *, round(NULLIF((profit/amount_per_sku),0)::NUMERIC,2) AS margin
+	FROM step_2
+)
+SELECT * FROM step_3);
+
+-------------------------------------------------------------------------
+SELECT max(margin),
+min(margin)
+FROM amazon_margin;
+
 SELECT *
-FROM ecommerce_data
-WHERE qty = 0;
+FROM amazon_margin
+ORDER BY margin DESC;
